4f92c985a265abbac74d0de1205d40bfe0a754ca
Lukas Ecklmayr <l.ecklmayr@outlook.com>
2023-03-29 15:08:41 +0200

    Update Theme + Some Options

--- Microsoft.Powershell_profile.ps1
+++ Microsoft.Powershell_profile.ps1
@@ -1,5 +1,8 @@
+#PWSH
 # OH my Posh
-oh-my-posh --init --shell pwsh --config "C:\Users\ecklmayl\AppData\Local\Programs\oh-my-posh\themes\amro.omp.json" | Invoke-Expression
+oh-my-posh --init --shell pwsh --config "C:\Users\ecklmayl\AppData\Local\Programs\oh-my-posh\themes\emodipt-extend.omp.json" | Invoke-Expression
+
+#emodipt-extend
 
 # Modules to Install
 #Install-Module -Name Terminal-Icons
