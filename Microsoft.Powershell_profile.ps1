b56b28f88fe2b7942eb9819e5b0825a11cf90dcc
Lukas Ecklmayr <l.ecklmayr@outlook.com>
2023-05-16 15:07:53 +0200

    add config

--- Microsoft.Powershell_profile.ps1
+++ Microsoft.Powershell_profile.ps1
@@ -1,12 +1,14 @@
 #PWSH
 # OH my Posh
-oh-my-posh --init --shell pwsh --config "C:\Users\ecklmayl\AppData\Local\Programs\oh-my-posh\themes\emodipt-extend.omp.json" | Invoke-Expression
+# oh-my-posh --init --shell pwsh --config "C:\Users\ecklmayl\AppData\Local\Programs\oh-my-posh\themes\emodipt-extend.omp.json" | Invoke-Expression
+oh-my-posh --init --shell pwsh --config "C:\Users\ecklmayl\AppData\Local\Programs\oh-my-posh\themes\lukas.json" | Invoke-Expression
+
 
 #emodipt-extend
 
 # Modules to Install
-#Install-Module -Name Terminal-Icons
-#Install-Module -Name PSReadline
+# Install-Module -Name Terminal-Icons
+# Install-Module -Name PSReadline
 
 # Load Modules
 Import-Module -Name Terminal-Icons
